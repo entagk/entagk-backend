89c36d17f7b9424f67035cfe9da54bf71afb61e4
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2022-11-15 01:55:25 +0200

    Write unit tests for User API controllers

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,12 +4,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const Auth = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
   try {
+    let token;
+    if (req?.headers?.authorization) {
+      token = req?.headers?.authorization?.split(" ")[1];
+    }
 
-    if (!token) return res.status(400).json({ message: "Invalid Authentication." });
+    if (!token) return res.status(401).json({ message: "Invalid Authentication." });
 
     const isCustomAuth = token.length < 500;
+
+    console.log(token, req?.headers?.authorization);
 
     let decodedData;
 
@@ -23,10 +28,15 @@ const Auth = (req, res, next) => {
       req.userId = decodedData?.sub;
     }
 
+
     next();
   } catch (error) {
-    res.status(500).json({ message: error.message, from: "Auth.js", token });
-    console.error(error); 
+    console.log(error);
+    if (error.name !== 'JsonWebTokenError') {
+      res.status(500).json({ message: error.message, from: "Auth.js", token });
+    } else {
+      res.status(401).json({ message: error.message, error });
+    }
   }
 }
 
