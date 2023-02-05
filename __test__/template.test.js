20e08e0c0e9e62450d52f21303078492407f1e59
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-02-05 23:09:42 +0200

    test: write getOne controller

--- __test__/template.test.js
+++ __test__/template.test.js
@@ -239,4 +239,77 @@ describe("Template APIs", () => {
         })
     })
   })
+
+  describe("Testing getOne controller", () => {
+    it("get public template", (done) => {
+      supertest(app)
+        .get(`/api/template/one/${templateData[0]._id}`)
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
+
+          expect(res.body).toEqual(templateData[0]);
+
+          done();
+        });
+    });
+
+    it("get private template", (done) => {
+      supertest(app)
+        .get(`/api/template/one/private/${templateData[1]._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
+
+          expect(res.body).toEqual(templateData[1]);
+
+          done();
+        });
+    });
+
+    it("send invalid id using route /api/template/one/privite/djs21dsf", (done) => {
+      supertest(app)
+        .get(`/api/template/one/private/djs21dsf`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(400)
+        .end((err, res) => {
+          if (err) throw err;
+
+          expect(res.body.message).toBe("Invalid id");
+
+          done();
+        });
+    });
+
+    it("send not found template using route /api/template/one/privite/63e0180ebb43b174201482a0", (done) => {
+      supertest(app)
+        .get(`/api/template/one/private/63e0180ebb43b174201482a0`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(404)
+        .end((err, res) => {
+          if (err) throw err;
+
+          expect(res.body.message).toBe("This template doesn't found.");
+
+          done();
+        });
+    });
+
+    it("get private template using route /api/template/one/63e0180ebb43b174201482a0", (done) => {
+      supertest(app)
+        .get(`/api/template/one/${templateData[1]._id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(405)
+        .end((err, res) => {
+          if (err) throw err;
+
+          expect(res.body.message).toBe("Not allow for you.");
+
+          done();
+        });
+    });
+  });
+
+  
 })
