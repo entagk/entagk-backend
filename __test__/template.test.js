31eadf51990f978a3e268043562c76e83bf2c092
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-02-06 17:26:49 +0200

    test: geting templates using getForUser controller

--- __test__/template.test.js
+++ __test__/template.test.js
@@ -416,4 +416,53 @@ describe("Template APIs", () => {
         });
     });
   });
+
+  describe("Testing getting user templates using route /api/template/user/", () => {
+    it("Send request without token", (done) => {
+      supertest(app)
+        .get("/api/template/user")
+        .expect(401)
+        .end((err, res) => {
+          if (err) throw err;
+
+          expect(res.body.message).toBe("Invalid Authentication.");
+
+          done();
+        })
+    });
+
+    it("Send request with invalid token", (done) => {
+      supertest(app)
+        .get("/api/template/user")
+        .set("Authorization", `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QwMTIzQGV4YW1wbGUuY29tIiwiaWQiOiI2MzhjMTE2NDI1ZGI3OGI1MGJjYzFjMDgiLCJpYXQiOjE2NzEwMzE5NzMsImV4cCI6MTY3MTA4OTU3M30.6lafCmgJDX393gGNakvwPbprgCHvrvVIXxUC3wSmxMg`)
+        .expect(401)
+        .end((err, res) => {
+          if (err) throw err;
+
+          expect(res.body.message).toBe("Invalid Authentication and jwt expired");
+
+          done();
+        })
+    });
+
+    it("Valid User", (done) => {
+      supertest(app)
+        .get("/api/template/user")
+        .set("Authorization", `Bearer ${token}`)
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
+
+          expect(res.body.total).toBe(templateData.length);
+          expect(res.body.currentPage).toBe(res.body.total === 0 ? 0 : 1);
+          expect(res.body.numberOfPages).toBe(Math.ceil(res.body.total / 25));
+
+          expect(res.body.templates).toEqual(templateData);
+
+          done();
+        })
+    });
+  });
+
+  
 })
