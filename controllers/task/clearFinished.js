9c9fafbf3ecbff6c339cad30a3ce406e43db4b92
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-06-21 20:01:33 +0300

    fix: clearFinished controller to delete checked todo template with their tasks

--- controllers/task/clearFinished.js
+++ controllers/task/clearFinished.js
@@ -6,7 +6,30 @@ const clearFinished = async (req, res) => {
   try {
     const userId = req.user._id.toString();
 
-    const results = await Task.deleteMany({ userId: userId, check: true, template: null, tasks: { $eq: [] } });
+    const results = await Task.deleteMany({ 
+      $or: [
+        {
+          userId: userId, 
+          check: true, 
+          template: null,
+          setting: null,
+          tasks: { $eq: [] }
+        },
+        {
+          userId: userId, 
+          check: true, 
+          "template.todo": true,
+          setting: null,
+          tasks: { $eq: [] }
+        },
+        {
+          userId: userId, 
+          check: true, 
+          setting: { $ne: null },
+          tasks: { $ne: [] },
+        }
+      ]
+    });
 
     res.status(200).json({ ...results, message: "Success deleted." });
   } catch (error) {
