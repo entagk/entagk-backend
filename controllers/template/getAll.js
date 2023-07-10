c17044699ff3750344d2292248213177a5bb48ee
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-07-11 01:40:08 +0300

    add sort property to sort all templates

--- controllers/template/getAll.js
+++ controllers/template/getAll.js
@@ -1,13 +1,13 @@
 const Template = require("../../models/template");
 
 const getAll = async (req, res) => {
-  const { page } = req.query || 1;
+  const { page, sort } = req.query || { page: 1, sort: 'updatedAt'};
   try {
     const limit = 25;
     const startIndex = (Number(page) - 1) * limit;
 
     const total = await Template.countDocuments({ visibility: { $eq: true } });
-    const templates = await Template.find({ visibility: { $eq: true } }).limit(limit).skip(startIndex);
+    const templates = await Template.find({ visibility: { $eq: true } }).sort({ [sort]: 1 }).limit(limit).skip(startIndex);
 
     res.status(200).json({
       templates,
