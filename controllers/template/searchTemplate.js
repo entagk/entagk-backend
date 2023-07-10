15123f47f12685c8e8d258d9c11cea417501976e
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-07-11 01:37:10 +0300

    add sort query for sorting search results

--- controllers/template/searchTemplate.js
+++ controllers/template/searchTemplate.js
@@ -2,24 +2,24 @@ const Template = require('../../models/template.js');
 
 const searchTemplates = async (req, res) => {
   try {
-    const { search } = req.query;
+    const { search, sort, page } = req.query || { search: "", sort: "updatedAt", page: 1 };
     console.log(search);
-    const { page } = req.query || 1;
-    const limit = 10;
+    console.log(sort);
+    console.log(page);
+    const limit = 25;
     const startIndex = (Number(page) - 1) * limit;
 
     if (search.trim().length === 0) return res.status(400).json({ message: "no search query, try again please" });
 
     const keys = search.split(" ").map(e => new RegExp(e, 'gi'));
-    console.log(keys);
 
     let total, templates;
-    if(req?.user?._id?.toString()) {
+    if (req?.user?._id?.toString()) {
       total = await Template.countDocuments({ $or: [{ name: { $in: keys } }, { desc: { $in: keys } }], userId: req?.user?._id?.toString() });
-      templates = await Template.find({ $or: [{ name: { $in: keys } }, { desc: { $in: keys } }], userId: req?.user?._id?.toString() }).limit(limit).skip(startIndex);
+      templates = await Template.find({ $or: [{ name: { $in: keys } }, { desc: { $in: keys } }], userId: req?.user?._id?.toString() }).sort({ [sort]: 1 }).limit(limit).skip(startIndex);
     } else {
       total = await Template.countDocuments({ $or: [{ name: { $in: keys } }, { desc: { $in: keys } }], visibility: true });
-      templates = await Template.find({ $or: [{ name: { $in: keys } }, { desc: { $in: keys } }], visibility: true }).limit(limit).skip(startIndex);
+      templates = await Template.find({ $or: [{ name: { $in: keys } }, { desc: { $in: keys } }], visibility: true }).sort({ [sort]: 1 }).limit(limit).skip(startIndex);
     }
 
     res
