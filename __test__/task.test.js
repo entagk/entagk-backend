412fb2b1a3b438bb2e4609d9bf5f5813689c8780
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-08-07 19:30:13 +0300

    fix: add multiple tasks controller tests

--- __test__/task.test.js
+++ __test__/task.test.js
@@ -148,117 +148,117 @@ describe("Task APIs", () => {
     });
   });
 
-  // describe("Testing addMultipleTasks controller throug route /api/task/add_multiple_tasks", () => {
-  //   it("Send request with object data", (done) => {
-  //     supertest(app)
-  //       .post('/api/task/add_multiple_tasks')
-  //       .set("Authorization", `Bearer ${token}`)
-  //       .send({ "jj": "jjj", "kk": "kkk" })
-  //       .expect(400)
-  //       .end((err, res) => {
-  //         if (err) throw err;
+  describe("Testing addMultipleTasks controller throug route /api/task/add_multiple_tasks", () => {
+    it("Send request with object data", (done) => {
+      supertest(app)
+        .post('/api/task/add_multiple_tasks')
+        .set("Authorization", `Bearer ${token}`)
+        .send({ "jj": "jjj", "kk": "kkk" })
+        .expect(400)
+        .end((err, res) => {
+          if (err) throw err;
 
-  //         expect(res.body.message).toBe("The data have been sent is not array, please try again");
+          expect(res.body.message).toBe("The data have been sent is not array, please try again");
 
-  //         done();
-  //       })
-  //   });
+          done();
+        })
+    });
 
-  //   it("Send request with empty data", (done) => {
-  //     supertest(app)
-  //       .post('/api/task/add_multiple_tasks')
-  //       .set("Authorization", `Bearer ${token}`)
-  //       .send([])
-  //       .expect(400)
-  //       .end((err, res) => {
-  //         if (err) throw err;
+    it("Send request with empty data", (done) => {
+      supertest(app)
+        .post('/api/task/add_multiple_tasks')
+        .set("Authorization", `Bearer ${token}`)
+        .send([])
+        .expect(400)
+        .end((err, res) => {
+          if (err) throw err;
 
-  //         expect(res.body.message).toBe("No data have been sent yet.");
+          expect(res.body.message).toBe("No data have been sent yet.");
 
-  //         done();
-  //       })
-  //   });
+          done();
+        })
+    });
 
-  //   it("Sending invalid est", (done) => {
-  //     supertest(app)
-  //       .post('/api/task/add_multiple_tasks')
-  //       .set("Authorization", `Bearer ${token}`)
-  //       .send([{ name: "test1", est: -1 }])
-  //       .expect(400)
-  //       .end((err, res) => {
-  //         if (err) throw err;
+    it("Sending invalid est", (done) => {
+      supertest(app)
+        .post('/api/task/add_multiple_tasks')
+        .set("Authorization", `Bearer ${token}`)
+        .send([{ name: "test1", est: -1 }])
+        .expect(400)
+        .end((err, res) => {
+          if (err) throw err;
 
-  //         expect(res.body.message).toBe("For task 1, The est shouldn't be negative number.");
+          expect(res.body.errors.est).toBe("For task 1, The est shouldn't be negative number.");
 
-  //         done();
-  //       })
-  //   })
+          done();
+        })
+    })
 
-  //   it("Sending invalid name", (done) => {
-  //     supertest(app)
-  //       .post('/api/task/add_multiple_tasks')
-  //       .set("Authorization", `Bearer ${token}`)
-  //       .send([{ name: "test1 test1 test1 test1 test1 test1 test1 test1 test1 test1", est: 2 }])
-  //       .expect(400)
-  //       .end((err, res) => {
-  //         if (err) throw err;
+    it("Sending invalid name", (done) => {
+      supertest(app)
+        .post('/api/task/add_multiple_tasks')
+        .set("Authorization", `Bearer ${token}`)
+        .send([{ name: "test1".repeat(50), est: 2 }])
+        .expect(400)
+        .end((err, res) => {
+          if (err) throw err;
 
-  //         expect(res.body.message).toBe("For task 1, The name length is more than 50 characters.");
+          expect(res.body.errors.name).toBe("For task 1, The name length is more than 50 characters.");
 
-  //         done();
-  //       })
-  //   })
+          done();
+        })
+    })
 
-  //   it("Sending invalid notes", (done) => {
-  //     supertest(app)
-  //       .post('/api/task/add_multiple_tasks')
-  //       .set("Authorization", `Bearer ${token}`)
-  //       .send([{ name: "test1", est: 2, notes: "test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1 test1" }])
-  //       .expect(400)
-  //       .end((err, res) => {
-  //         if (err) throw err;
+    it("Sending invalid notes", (done) => {
+      supertest(app)
+        .post('/api/task/add_multiple_tasks')
+        .set("Authorization", `Bearer ${token}`)
+        .send([{ name: "test1", est: 2, notes: "test1".repeat(500) }])
+        .expect(400)
+        .end((err, res) => {
+          if (err) throw err;
 
-  //         expect(res.body.message).toBe("For task 1, The notes length is more than 500 characters.");
+          expect(res.body.errors.notes).toBe("For task 1, The notes length is more than 500 characters.");
 
-  //         done();
-  //       })
-  //   })
+          done();
+        })
+    })
 
-  //   it("Sending valid data", (done) => {
-  //     supertest(app)
-  //       .post('/api/task/add_multiple_tasks')
-  //       .set("Authorization", `Bearer ${token}`)
-  //       .send([{ ...taskData[0], check: false }])
-  //       .expect(200)
-  //       .end((err, res) => {
-  //         if (err) throw err;
+    it("Sending valid data", (done) => {
+      supertest(app)
+        .post('/api/task/add_multiple_tasks')
+        .set("Authorization", `Bearer ${token}`)
+        .send([{ ...taskData[0], check: false }])
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
 
-  //         const data = res.body[0];
+          const data = res.body[0];
 
-  //         test(data, { userId, check: false, notes: taskData[0].notes, act: 0, est: taskData[0].est, name: taskData[0].name });
+          test(data, { userId, check: false, notes: taskData[0].notes, act: 0, est: taskData[0].est, name: taskData[0].name });
 
-  //         taskData[1] = Object.assign(taskData[0], data);
+          taskData[1] = Object.assign(taskData[0], data);
 
-  //         done();
-  //       })
-  //   })
+          done();
+        })
+    })
 
-  //   it("get all tasks", (done) => {
-  //     supertest(app)
-  //       .get("/api/task/")
-  //       .set("Authorization", `Bearer ${token}`)
-  //       .expect(200)
-  //       .end((err, res) => {
-  //         if (err) throw err;
+    it("get all tasks", (done) => {
+      supertest(app)
+        .get("/api/task/")
+        .set("Authorization", `Bearer ${token}`)
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
 
-  //         const data = res.body;
+          const data = res.body;
 
-  //         test({ ...data, tasks: data.tasks[1] }, { tasks: taskData[1], total: taskData.length, currentPage: 1, numberOfPages: 1 });
+          test({ ...data, tasks: data.tasks[1] }, { tasks: taskData[1], total: taskData.length, currentPage: 1, numberOfPages: 1 });
 
-  //         done();
-  //       })
-  //   });
-  // })
+          done();
+        })
+    });
+  })
 
   // describe("Testing updateTask controller route /api/task/update/:id", () => {
   //   it("Send request with invalid id", (done) => {
