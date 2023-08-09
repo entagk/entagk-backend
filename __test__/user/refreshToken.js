27a887ad96501f9a09758ec48ea53c0d3feb005c
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-08-09 21:33:06 +0300

    fix: refreshToken tests issues

---
+++ __test__/user/refreshToken.js
@@ -0,0 +1,33 @@
+const supertest = require('supertest');
+const app = require('../../server');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose')
+
+const { getData, setData } = require("./utils");
+
+module.exports = () => describe("Testing refreshToken GET route /api/user/refresh_token", () => {
+  require('./validateAuth')("/api/user/refresh_token");
+
+  it("Sending valid request", (done) => {
+    supertest(app)
+      .get('/api/user/refresh_token')
+      .set('Authorization', `Bearer ${getData('token')}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+
+        expect(res.body.message).toBe("You are logged in successfully");
+        if (res.body.refresh_token.length < 500) {
+          const tokenData = jwt.verify(res.body.refresh_token, process.env.ACCESS_TOKEN_SECRET);
+          expect(mongoose.Types.ObjectId.isValid(tokenData.id)).toBe(true);
+          setData("token", res.body.refresh_token)
+        } else {
+          const tokenData = jwt.decode(res.body.refresh_token);
+          expect(mongoose.Types.ObjectId.isValid(tokenData.sub)).toBe(true);
+          setData("token", res.body.refresh_token)
+        }
+
+        done();
+      })
+  })
+});
--- __test__/user/user.test.js
+++ __test__/user/user.test.js
@@ -26,63 +26,7 @@ describe('User APIs', () => {
 
   require('./resetPassword')();
 
-  // describe("Testing refreshToken GET route /api/user/refresh_token", () => {
-  //   it("Testing sending request without token", (done) => {
-  //     supertest(app)
-  //       .get('/api/user/refresh_token')
-  //       .expect(401)
-  //       .end((err, res) => {
-  //         if (err) throw err;
-
-  //         expect(res.body.message).toBe("Invalid Authentication.");
-  //         done();
-  //       })
-  //   });
-
-  //   it("Send request with invalid token", (done) => {
-  //     supertest(app)
-  //       .get('/api/user/refresh_token')
-  //       .set("Authorization", `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QwMTIzQGV4YW1wbGUuY29tIiwiaWQiOiI2MzhjMTE2NDI1ZGI3OGI1MGJjYzFjMDgiLCJpYXQiOjE2NzEwMzE5NzMsImV4cCI6MTY3MTA4OTU3M30.6lafCmgJDX393gGNakvwPbprgCHvrvVIXxUC3wSmxMg`)
-  //       .expect(401)
-  //       .end((err, res) => {
-  //         if (err) throw err;
-
-  //         expect(res.body.message).toBe("Invalid Authentication and jwt expired");
-
-  //         done();
-  //       })
-  //   });
-
-  //   it("Send request with invalid token of fack userId", (done) => {
-  //     const fakeToken = createAcessToken({ email: userData.email, id: userData?.id });
-  //     supertest(app)
-  //       .get('/api/user/refresh_token')
-  //       .set("Authorization", `Bearer ${fakeToken}`)
-  //       .expect(401)
-  //       .end((err, res) => {
-  //         if (err) throw err;
-
-  //         expect(res.body.message).toBe("Invalid Authentication");
-
-  //         done();
-  //       })
-  //   });
-
-  //   it("Send request with invalid token of not found user", (done) => {
-  //     const fakeToken = createAcessToken({ email: "wjdjsifk@jfdd.com", id: '641ee1ba92578ebf24203deb' });
-  //     supertest(app)
-  //       .get('/api/user/refresh_token')
-  //       .set("Authorization", `Bearer ${fakeToken}`)
-  //       .expect(404)
-  //       .end((err, res) => {
-  //         if (err) throw err;
-
-  //         expect(res.body.message).toBe("user not found");
-
-  //         done();
-  //       })
-  //   });
-  // });
+  require('./refreshToken')();
 
   // describe("Testing updateUser controller route /api/user/update_user", () => {
   //   it("Sending invalid token", (done) => {
