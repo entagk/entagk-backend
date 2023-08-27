1a06b53291141ba342eb03636d37a9f270076b5d
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-08-27 11:18:46 +0300

    Handle getting month days with RegExp

--- controllers/activeDay/getDays.js
+++ controllers/activeDay/getDays.js
@@ -1,30 +1,30 @@
 const ActiveDay = require('../../models/active');
 const { validateDate } = require('../../utils/helper');
 
-const calcDays = (start, end) => {
+const calcDifferent = (start, end) => {
   const startDate = new Date(start);
   const endDate = new Date(end);
 
   const different = (endDate - startDate) / 1000 / 60 / 60 / 24;
-  if (different < 0) {
-    return { message: 'Invalid start and end days' };
-  } else if (different === 1) {
-    return { days: [start, end] };
-  } else {
-    const days = [start, end,];
-    for (let i = 1; i < different; i++) {
-      const date = new Date(start);
-      date.setDate(date.getDate() + i);
-      days.push(date.toJSON().split('T')[0]);
-    }
+  return different;
+}
 
-    return { days: days };
+const calcDays = (start, end, different) => {
+  const days = [start, end,];
+  for (let i = 1; i < different; i++) {
+    const date = new Date(start);
+    date.setDate(date.getDate() + i);
+    days.push(date.toJSON().split('T')[0]);
   }
+
+  return { days: days };
 }
 
 const getDays = async (req, res) => {
   try {
     const { start, end } = req.query;
+    const startDate = new Date(start);
+    const endDate = new Date(end);
 
     if (!start && !end) return res.status(400).json({ message: "Please, enter the start and the end days" });
 
@@ -40,11 +40,26 @@ const getDays = async (req, res) => {
 
       res.status(200).json(daysData);
     } else {
-      const days = calcDays(start, end);
+      const different = calcDifferent(start, end);
+      if (different < 0) {
+        return res.status(400).json({ message: 'Invalid start and end days' });
+      } else if (different > 31) {
+        return res.status(400).json({ message: "Sorry, the limit is 31 days." });
+      } else if (
+        startDate.getMonth() === endDate.getMonth() &&
+        startDate.getDate() === new Date(endDate.setDate(endDate.getDate() + 1)).getDate()
+      ) {
+        const monthRegExp = new RegExp(start.slice(0, 7));
 
-      if (days?.message) {
-        res.status(400).json({ message: days.message });
+        const daysData = await ActiveDay.find({
+          day: monthRegExp,
+          userId: req.user._id.toString()
+        })
+
+        res.status(200).json(daysData);
       } else {
+        const days = calcDays(start, end, different);
+
         const daysData = await ActiveDay
           .find
           (
@@ -54,11 +69,9 @@ const getDays = async (req, res) => {
             }
           );
 
-        console.log(days);
-        console.log(daysData);
-
         res.status(200).json(daysData);
       }
+
     }
   } catch (error) {
     console.log(error);
