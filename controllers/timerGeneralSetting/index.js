cd7f7f2ade83fec53e334109da96bc46655feaa0
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-11-17 23:35:06 +0200

    create a controller to get general sounds (alarm, ticking, and click)

--- controllers/timerGeneralSetting/index.js
+++ controllers/timerGeneralSetting/index.js
@@ -1,6 +1,7 @@
 const SettingControllers = {
   getSetting: require('./getSetting'),
   update: require('./updateSetting'),
+  getGeneralSounds: require('./getGeneralSounds')
 };
 
 module.exports = SettingControllers;
